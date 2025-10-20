8884cfe13f3a805d3b0615a9164b9c1b40f62b3d
Tolojanahary Ranarijaonina <tolojaranarijaonina@gmail.com>
2025-10-21 02:12:37 +0300

    Commande form

--- templates/main.js
+++ templates/main.js
@@ -1,3 +1,4 @@
+// Bares caché
 const offcave = document.getElementById("offcave");
 const listes = document.querySelector(".listes");
 
@@ -5,10 +6,46 @@ offcave.addEventListener('click', () => {
     listes.classList.toggle("menu-list")
 })
 
+//VOIR PLUS/MOINS
+const voirPlus = document.querySelector(".voirPlus");
+const voirMoins = document.querySelector(".voirMoins");
+
+voirPlus.addEventListener("click", (e) => {
+    e.target.parentElement.querySelector("p").textContent += `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Repellendus laudantium ipsa aperiam iure nulla, deserunt dolor harum, nesciunt autem fugiat atque. 
+Deleniti ipsum non pariatur sunt magnam, nulla quo aspernatur!` ;
+    voirPlus.style.display = "none";
+    voirMoins.style.display = "block";
+})
+
+
+voirMoins.addEventListener("click", (e) => {
+    e.target.parentElement.querySelector("p").textContent = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis illum sunt perferendis odio eum saepe
+                consequatur reprehenderit magni in odit.`;
+    voirPlus.style.display = "block";
+    voirMoins.style.display = "none";
+})
+
 // BOUTTON COMMANDE
 const main = document.getElementById("main");
 const service = document.getElementById("service");
 const commandeForm = document.getElementById('commandeForm');
+
+// RECUPERATION DES DONNES
+
+const connexion = document.getElementById("connexion");
+connexion.addEventListener("submit", () => {
+    localStorage.setItem("nom",document.getElementById("nom").value);
+    localStorage.setItem("numeroTel",document.getElementById("numeroTel").value);
+    window.location.href = "https://toloja2.github.io/SiteAppSuchi/templates/index.html";
+
+})
+
+const nomPerso = document.getElementById("nomPerso");
+nomPerso.value = localStorage.getItem("nom");
+const numeroTelPerso = document.getElementById("numeroTelPerso");
+numeroTelPerso.value = localStorage.getItem("numeroTel");
+
 
 main.addEventListener("click", (e) => {
     // verifions si on clique avec un document avec class bouttonCommander
@@ -24,14 +61,14 @@ main.addEventListener("click", (e) => {
         main.style.display = "none";
     }
 
-    
+
 })
 
 // Voir autre commande
-    const voirAutreCommande = document.getElementById('voirAutreCommande');
-    voirAutreCommande.addEventListener("click", () => {
-        commandeForm.style.display = "none";
-        main.style.display = "block";
+const voirAutreCommande = document.getElementById('voirAutreCommande');
+voirAutreCommande.addEventListener("click", () => {
+    commandeForm.style.display = "none";
+    main.style.display = "block";
 
 
-    })
+})
